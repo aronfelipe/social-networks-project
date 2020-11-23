ad7dd36ed054d671233fe52f1aff6bbf4ceee226
felipe <aronakamoto@outlook.com>
2020-11-23 11:38:06 -0300

    Fix: Get block height

--- DataAPI/app.py
+++ DataAPI/app.py
@@ -32,37 +32,38 @@ class App:
             t0 = time.time()
             try:
                 block = self.data.get_block_height(n_block)
-            except Exception as e:
-                print(e)
-                print
-            self.generate_volatility(block['blocks'][0]['time'])
-            counter_block = counter_block + 1
-            counter = 0
-            for transaction in block['blocks'][0]['tx']:
-                counter = counter + 1
-                for transaction_input in transaction['inputs']:
-                    for transaction_output in transaction['out']:
-                        try:
-                            self.report.write_line(transaction_input['prev_out']['addr'], transaction_output['addr'], transaction['time'])
-                            t1 = time.time()
-                            total = t1-t0
-                            if total > 60:
-                                break
-                        except:
-                            t1 = time.time()
-                            total = t1-t0
-                            if total > 60:
-                                break
-                            
+                self.generate_volatility(block['blocks'][0]['time'])
+                counter_block = counter_block + 1
+                counter = 0
+                for transaction in block['blocks'][0]['tx']:
+                    counter = counter + 1
+                    for transaction_input in transaction['inputs']:
+                        for transaction_output in transaction['out']:
+                            try:
+                                self.report.write_line(transaction_input['prev_out']['addr'], transaction_output['addr'], transaction['time'])
+                                t1 = time.time()
+                                total = t1-t0
+                                if total > 60:
+                                    break
+                            except:
+                                t1 = time.time()
+                                total = t1-t0
+                                if total > 60:
+                                    break
+                                
+                        t1 = time.time()
+                        total = t1-t0
+                        if total > 60:
+                            break
+
                     t1 = time.time()
                     total = t1-t0
                     if total > 60:
                         break
 
-                t1 = time.time()
-                total = t1-t0
-                if total > 60:
-                    break
+                self.report.to_xlsx(n_block)
+            except Exception as e:
+                print(e)
+                print
 
-            self.report.to_xlsx(n_block)
 
